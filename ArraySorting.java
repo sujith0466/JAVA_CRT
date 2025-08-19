b8a6b10da625f4fb0d56d62bec6d6eb87608a918
Sujith Kumar Sanisetty <sujithsunil4818@gmail.com>
2025-08-19 17:19:45 +0530

    Update and rename Array_operations.java to ArraySorting.java

---
+++ ArraySorting.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class ArraySorting {
+    public static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) System.out.print(num + " ");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        bubbleSort(arr);
+        System.out.println("Sorted array:");
+        printArray(arr);
+    }
+}
